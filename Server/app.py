a5d42564814c877732720903477b5d741ebdac79
mihir-b-shah <46288414+mihir-b-shah@users.noreply.github.com>
2019-11-14 00:23:08 -0600

    update server

--- Server/app.py
+++ Server/app.py
@@ -1,13 +1,22 @@
+
 from flask import Flask, jsonify, request
 from nltk.corpus import wordnet as wn
 
 app = Flask(__name__)
 
-@app.route('/ss/', methods=['GET'])
-def get_task():
-    w1 = request.args.get('w1',"",type=str)
-    w2 = request.args.get('w2',"",type=str)
+def word_pathsim(w1,w2):
     return str((wn.synsets(str(w1),'n')[0]).path_similarity(wn.synsets(str(w2),'n')[0]))
 
+@app.route('/semsim', methods = ['POST']) 
+def gen_ss():
+    word_data = request.get_json()
+    print(type(word_data))
+    w1 = word_data["w1"]
+    w2 = word_data["w2"]
+    val = word_pathsim(w1,w2)
+    res = {'status':val}
+    return jsonify(res)
+
 if __name__ == '__main__':
+    app.run()
     app.run(debug=True)
---
+++ Server/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+print("Content-type: text/html")
+print("")
+
+print("<html>")
+print("<body>")
+
+print( "Hi there")
+
+print("</body>")
+print("</html>") 
